675448a89aac2867f44523126fa141cb87b15f1d
vrossi96 <vrossi396@yahoo.it>
2022-02-18 15:13:07 +0100

    add first imgs to slider

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,17 +1,24 @@
 <template>
-   <div class="slider container">
-      <div class="row">
-         <div class="col-5 offset-1 d-flex flex-column justify-content-center">
-            <h1>Devotion that never <i>ends</i></h1>
-            <p class="text-secondary py-4">
-               Aperiam, similique earum. Illo alias accusamus corrupti magni quibusdam autem odio nihil. Odio eius pariatur consectetur voluptas
-               voluptates placeat reprehenderit laudantium repellat!
-            </p>
-            <div>
-               <a class="p-btn text-uppercase" href="#">Read more</a>
+   <div>
+      <div class="slider container">
+         <div class="row">
+            <!-- Left side TEXT AREA -->
+            <div class="col-5 offset-1 d-flex flex-column justify-content-center">
+               <h1>Devotion that never <i>ends</i></h1>
+               <p class="text-secondary py-4">
+                  Aperiam, similique earum. Illo alias accusamus corrupti magni quibusdam autem odio nihil. Odio eius pariatur consectetur voluptas
+                  voluptates placeat reprehenderit laudantium repellat!
+               </p>
+               <div>
+                  <a class="p-btn text-uppercase" href="#">Read more</a>
+               </div>
+            </div>
+            <!-- RIGHT side IMG AREA -->
+            <div class="img-slider col-5 offset-1 d-flex align-items-center justify-content-center">
+               <img src="../assets/images/img/h-2-slider-img-15.png" alt="" />
+               <img src="../assets/images/img/h-2-slider-img-16.png" alt="" />
             </div>
          </div>
-         <div class="col-5"></div>
       </div>
    </div>
 </template>
@@ -28,8 +35,9 @@ export default {
 
    .col-5 {
       border: 1px dotted red;
-      height: 500px;
+      // height: 500px;
 
+      // LEFT SIDE-TEXT SECTION
       h1 {
          font-size: 75px;
          i {
@@ -42,11 +50,19 @@ export default {
       a {
          text-decoration: none;
          font-size: 12px;
+         font-weight: 700;
          color: #000;
       }
       .p-btn {
          padding: 15px 30px;
          border: 2px solid black;
+      }
+   }
+
+   // RIGHT SIDE-IMG SECTION
+   .img-slider {
+      img {
+         height: 80%;
       }
    }
 }
