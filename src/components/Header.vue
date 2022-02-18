1fcfe6cea75bea6db135525b8fd635762576fbc5
vrossi96 <vrossi396@yahoo.it>
2022-02-18 12:16:50 +0100

    added header

---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+   <div class="container-fluid" id="header">
+      <div class="nav row h-100 align-items-center justify-content-between">
+         <div class="col flex-grow-0">
+            <img role="button" src="../assets/img/logo-sidearea-1.png" alt="Logo" />
+         </div>
+         <ul class="col flex-grow-0">
+            <li role="button" v-for="item in links" :key="item.name" class="text-uppercase mx-3 d-flex align-items-center">
+               <span v-if="!item.img">{{ item.name }}</span>
+               <img v-else :src="require(`../assets/svg/${item.img}`)" :alt="item.name" />
+            </li>
+         </ul>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+   name: "Header",
+   props: {},
+   data() {
+      return {
+         links: [
+            { name: "home", url: "#", img: "" },
+            { name: "pages", url: "#", img: "" },
+            { name: "portfolio", url: "#", img: "" },
+            { name: "blog", url: "#", img: "" },
+            { name: "shop", url: "#", img: "" },
+            { name: "elements", url: "#", img: "" },
+            { name: "more", url: "#", img: "svg-1.svg" },
+         ],
+      };
+   },
+};
+</script>
+
+<style scoped lang="scss">
+#header {
+   height: 96px;
+
+   // todo remove
+   border-bottom: 1px solid black;
+
+   ul {
+      list-style: none;
+      display: flex;
+      margin-bottom: 0;
+   }
+}
+</style>
