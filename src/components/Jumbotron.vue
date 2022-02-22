02f370e5df26cbcf2716c9a795f789ecb37f4a24
vrossi96 <vrossi396@yahoo.it>
2022-02-22 14:47:28 +0100

    add hover effect in jumbotron

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -2,11 +2,18 @@
    <div>
       <div class="row">
          <div
-            class="col-4 jum-block"
+            class="col-4 jum-block d-flex align-items-center"
             v-for="(image, index) in jumboCards"
             :key="index"
             :style="{ backgroundImage: `url(${require('../assets/images/img/' + image.img)})` }"
-         ></div>
+         >
+            <div class="row">
+               <div class="col box-label">
+                  <h4>{{ image.title }}</h4>
+                  <i>{{ image.type }}</i>
+               </div>
+            </div>
+         </div>
       </div>
    </div>
 </template>
@@ -19,12 +26,12 @@ export default {
          test: "../assets/images/img/h-2-port-img-1.jpg",
          cardImgPath: "../assets/images/img/",
          jumboCards: [
-            { img: "h-2-port-img-1.jpg" },
-            { img: "h-2-port-img-2.jpg" },
-            { img: "h-2-port-img-3.jpg" },
-            { img: "h-2-port-img-4.jpg" },
-            { img: "h-2-port-img-5.jpg" },
-            { img: "h-2-port-img-6.jpg" },
+            { img: "h-2-port-img-1.jpg", title: "Letters", type: "Illustration" },
+            { img: "h-2-port-img-2.jpg", title: "Couple bycicle", type: "Movement" },
+            { img: "h-2-port-img-3.jpg", title: "US letters Brochure Template", type: "Books" },
+            { img: "h-2-port-img-4.jpg", title: "Illustration of novels", type: "Illustration" },
+            { img: "h-2-port-img-5.jpg", title: "Van Gogh phone wallpaper", type: "Phone art" },
+            { img: "h-2-port-img-6.jpg", title: 'Google "art" with letters', type: "Example" },
          ],
       };
    },
@@ -37,6 +44,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "../assets/scss/_mixins.scss";
+
 .col-4 {
    height: calc(100vw / 3);
 }
@@ -44,5 +53,24 @@ export default {
    background-size: cover;
    background-position: center;
    background-repeat: no-repeat;
+   cursor: pointer;
+
+   .box-label {
+      opacity: 0;
+      background-color: #fff;
+      padding: 15px 25px;
+      transition: 0.2s linear;
+
+      i {
+         font-family: "American Typewriter", serif;
+         font-weight: 600;
+      }
+   }
+
+   &:hover {
+      .box-label {
+         opacity: 1;
+      }
+   }
 }
 </style>
